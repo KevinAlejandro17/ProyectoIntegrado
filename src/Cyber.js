f8fcb6a34282bf6b2174ec760dc6d68f6e440e5d
Invitado <invitado@s1pc10.eisc.univalle.edu.co>
2023-03-31 10:53:24 -0500

    si

--- src/Cyber.js
+++ src/Cyber.js
@@ -3,25 +3,32 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export default function Cyber(props) {
-  const { nodes, materials } = useGLTF("/static/Robot.glb");
+  const { nodes, materials } = useGLTF("/static/Pyramid.glb");
   return (
-    <group {...props} >
-      <group rotation={[-Math.PI / 2, 0, 0]} scale={0.04} position-z={-8}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Object_2.geometry}
-          material={materials.StingrayPBS2SG}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Object_3.geometry}
-          material={materials.StingrayPBS3SG}
-        />
+    <group {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]} scale={0.0129314} position-y={-1}>
+        <group rotation={[Math.PI / 2, 0, 0]}>
+          <group
+            position={[0.71042252, 0, 0.05725479]}
+            rotation={[-Math.PI / 2, 0, 0]}
+          >
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes["Pyramid_02_-_Default_0"].geometry}
+              material={materials["02_-_Default"]}
+            />
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={nodes["Pyramid_01_-_Default_0"].geometry}
+              material={materials["01_-_Default"]}
+            />
+          </group>
+        </group>
       </group>
     </group>
   );
 }
 
-useGLTF.preload("/static/Robot.glb");
+useGLTF.preload("/static/Pyramid.glb");
--- src/Experience.js
+++ src/Experience.js
@@ -12,7 +12,7 @@ export default function Experience() {
         <directionalLight position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
 
-        <Burger />
+      
         <Cyber />
 
         <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={100}><planeGeometry /><meshStandardMaterial color="greenyellow" /></mesh>
