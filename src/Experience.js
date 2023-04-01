1c646a2827abde89129fab1bd37f8ac7f14bb28e
KevinAlejandro17 <89487705+KevinAlejandro17@users.noreply.github.com>
2023-04-01 00:19:53 -0500

    Add fox animation and sky

--- src/Experience.js
+++ src/Experience.js
@@ -1,21 +1,50 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
-import Burger from './Burger'
-import Pyramid from './Pyramid'
-import Alien from './Alien'
+import {
+  OrbitControls,
+  MeshReflectorMaterial,
+  Text,
+  Sky,
+  Stars,
+} from "@react-three/drei";
+import Fox from "./Fox";
 
 export default function Experience() {
-    return <>
-    
+  return (
+    <>
+      <OrbitControls makeDefault enablePan={false} maxPolarAngle={1.5} />
+      <Sky
+        distance={45000}
+        sunPosition={[1, -0.05, 1.2]}
+        inclination={0}
+        azimuth={0.25}
+        elevation={1}
+        rayleigh={3}
+        
+      />
+      <Stars
+        radius={100}
+        depth={50}
+        count={5000}
+        factor={4}
+        saturation={0}
+        fade
+        speed={1}
+      />
 
-        <OrbitControls makeDefault />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <directionalLight position={[1, 2, 3]} intensity={1.5} />
-        <ambientLight intensity={0.5} />
+      <Fox />
 
-        <Alien />
-        <Pyramid />
-
-        <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={100}><planeGeometry /><meshStandardMaterial color="greenyellow" /></mesh>
+      <mesh rotation-x={-Math.PI * 0.5} scale={100} position-y={-1}>
+        <planeGeometry />
+        <MeshReflectorMaterial
+          resolution={512}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          mirror={0.6}
+          color={"greenyellow"}
+        />
+      </mesh>
     </>
+  );
 }
---
+++ src/Fox.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+
+const Fox = (props) => {
+  const { animations, scene } = useGLTF("/static/Fox.glb");
+  const foxAnimations = useAnimations(animations, scene);
+
+  useEffect(() => {
+    const action = foxAnimations.actions.Walk;
+    action.play();
+  }, []);
+
+  return (
+    <primitive
+      object={scene}
+      scale={0.02}
+      position-y={-1}
+      rotation-y={8}
+    ></primitive>
+  );
+};
+export default Fox;
+useGLTF.preload("/static/Robot.glb");
