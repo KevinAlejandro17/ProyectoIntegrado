f8fa6d77716a84dae65fa228fa4c2b794767121c
KevinAlejandro17 <89487705+KevinAlejandro17@users.noreply.github.com>
2023-04-01 01:24:06 -0500

    Add pyramid, textures, sky and floating text

--- src/Experience.js
+++ src/Experience.js
@@ -1,21 +1,49 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
-import Burger from './Burger'
-import Pyramid from './Pyramid'
-import Alien from './Alien'
+import { Float, OrbitControls, Sky, Text, useTexture } from "@react-three/drei";
+import Pyramid from "./Pyramid";
 
 export default function Experience() {
-    return <>
-    
+  const texture = useTexture("/static/sand.jpg");
 
-        <OrbitControls makeDefault />
+  return (
+    <>
+      <OrbitControls
+        makeDefault
+        minDistance={55}
+        maxDistance={60}
+        minPolarAngle={1}
+        maxPolarAngle={1.5}
+      />
 
-        <directionalLight position={[1, 2, 3]} intensity={1.5} />
-        <ambientLight intensity={0.5} />
+      <Sky
+        distance={45000}
+        sunPosition={[1, -0.35, -10]}
+        inclination={0}
+        azimuth={0.25}
+        elevation={1}
+        rayleigh={3}
+      />
 
-        <Alien />
-        <Pyramid />
+      <directionalLight position={[1, 5, 1]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={100}><planeGeometry /><meshStandardMaterial color="greenyellow" /></mesh>
+      <Pyramid />
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={100}>
+        <planeGeometry />
+        <meshStandardMaterial map={texture} />
+      </mesh>
+
+      <Float speed={5}>
+          <Text
+            position={[0, 28, 5]}
+            scale={3}
+            font="/static/chakra-petch.woff"
+            fontSize={1.5}
+            color="white"
+          >
+            Civilización egipcia
+          </Text>
+      </Float>
     </>
+  );
 }
--- src/Pyramid.js
+++ src/Pyramid.js
@@ -1,32 +1,30 @@
-
 import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, useTexture } from "@react-three/drei";
 
 export default function Cyber(props) {
-  const { nodes, materials } = useGLTF("/static/Pyramid.glb");
+  const { nodes } = useGLTF("/static/Pyramid.glb");
+  const texture = useTexture("/static/texture.jpg");
+
   return (
-    <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]} scale={0.02} position-y={-1.3}>
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          <group
-            position={[0.71042252, 0, 0.05725479]}
-            rotation={[-Math.PI / 2, 0, 0]}
-          >
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={nodes["Pyramid_02_-_Default_0"].geometry}
-              material={materials["02_-_Default"]}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={nodes["Pyramid_01_-_Default_0"].geometry}
-              material={materials["01_-_Default"]}
-            />
-          </group>
-        </group>
-      </group>
+    <group
+      position-y={-4}
+      rotation={[-Math.PI / 2, 0, 0]}
+      scale={0.2}
+    >
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["Pyramid_02_-_Default_0"].geometry}
+      >
+        <meshStandardMaterial map={texture} />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["Pyramid_01_-_Default_0"].geometry}
+      >
+        <meshStandardMaterial map={texture} />
+      </mesh>
     </group>
   );
 }
